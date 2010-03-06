023a8bc30bffa052e2f08457a6d8a2f0aa27ae7f
Heinz N. Gies <heinz@licenser.net>
2010-03-06 02:18:00 +0100

    * Changed silly Clepic name to EPIC. The jar is now much cooler named

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject Clepic "1.0.0-SNAPSHOT" 
+(defproject EPIC "1.0.0-SNAPSHOT" 
 	:description "FIXME: write" 
 	:dependencies [[org.clojure/clojure "1.1.0"] 
 	[org.clojure/clojure-contrib "1.1.0"]] 
