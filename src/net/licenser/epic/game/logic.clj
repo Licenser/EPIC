e3a375ac7cb0be44acc74bea5cebc96ef149cb4f
Heinz N. Gies <heinz@licenser.net>
2010-03-08 20:06:00 +0100

    Fixed when let bug.

--- src/net/licenser/epic/game/logic.clj
+++ src/net/licenser/epic/game/logic.clj
@@ -111,12 +111,12 @@
 
 (defn emply-point-defense
   [game unit]
-  (when-let [pd-s  (filter #(re-find #"Point Defense" (:name %)) (utils/get-modules @unit :weapon))
-	     r (reduce #(max %1 %2) (map #(+ (utils/module-spec % :range) (utils/module-spec % :variation)) pd-s))
-	     ts (find-hostile-units game unit r)]
-    (doall (map (fn [w]
-		  (doall (map (fn [t] (fire-weapon game unit (:id w) t)) ts))) pd-s)))
-  game)
+  (when-let [pd-s  (filter #(re-find #"Point Defense" (:name %)) (utils/get-modules @unit :weapon))]
+    (let [r (reduce #(max %1 %2) (map #(+ (utils/module-spec % :range) (utils/module-spec % :variation)) pd-s))
+	  ts (find-hostile-units game unit r)]
+      (doall (map (fn [w]
+		    (doall (map (fn [t] (fire-weapon game unit (:id w) t)) ts))) pd-s))))
+    game)
 
 (defn fire-all [game unit target]
   (let [modules (map #(:id %) (utils/get-modules @unit :weapon))]
